75faf37b9f39d7ff8dec854165ee6e835b8e065e
ye-geeee <yeaji9683@gmail.com>
2021-01-15 07:25:21 +0900

    Initial commit

---
+++ src/main/java/com/yegeeee/moniapp/MoniAppApplication.java
@@ -0,0 +1,13 @@
+package com.yegeeee.moniapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoniAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoniAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yegeeee/moniapp/MoniAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yegeeee.moniapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoniAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
